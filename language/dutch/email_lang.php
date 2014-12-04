6e8d72b9964541121922686daf6050bb838666dc
Pieter Krul <pkrul@snow.nl>
2014-12-04 12:19:21 +0100

    Update email_lang

--- language/dutch/email_lang.php
+++ language/dutch/email_lang.php
@@ -39,25 +39,25 @@
  */
 defined('BASEPATH') OR exit('Directe toegang tot scripts is niet toegestaan');
 
-$lang['email_must_be_array'] = 'De validatiemethode voor email heeft er graag een array bij. Ja? Dus..';
-$lang['email_invalid_address'] = 'Dit emailadres wordt niet als zodanig beschouwd: %s';
-$lang['email_attachment_missing'] = 'Het lukt me niet om de bijlage die bij dit bericht hoort erbij te halen: %s';
-$lang['email_attachment_unreadable'] = 'Ik kan de bijlage die bij dit bericht hoort niet lezen. Ja? Dus: %s';
-$lang['email_no_from'] = 'Email versturen zonder "From"-header, dat doe ik niet. Ja?';
-$lang['email_no_recipients'] = 'Onthoudt: ontvangers (Naar, Cc, of Bcc) horen er ook bij.';
-$lang['email_send_failure_phpmail'] = 'Het lukt niet om email te versturen met PHP mail(). Deze server zou wel eens een andere methode gebruiken om berichten te sturen. Dus..';
-$lang['email_send_failure_sendmail'] = 'Het lukt niet om email te versturen met PHP Sendmail. Deze server zou wel eens een andere methode gebruiken om berichten te sturen. Dus..';
-$lang['email_send_failure_smtp'] = 'Het lukt niet om email te versturen met PHP SMTP.  Deze server zou wel eens een andere methode gebruiken om berichten te sturen. Dus..';
-$lang['email_sent'] = 'Het bericht is daadwerkelijk verstuurd, via het volgende protocol. Dus: %s';
-$lang['email_no_socket'] = 'To open a socket to sendmail unable.  Check settings.  Yes, hmmm.';
-$lang['email_no_hostname'] = 'Specify a smtp hostname, you did not.  ';
-$lang['email_smtp_error'] = 'Encountered, the following SMTP error was: %s';
-$lang['email_no_smtp_unpw'] = 'Error: a SMTP username and password assign you must.';
-$lang['email_failed_smtp_login'] = 'To send, AUTH LOGIN command failed. Error: %s';
-$lang['email_smtp_auth_un'] = 'Authenticate username, failed it did. Error: %s';
-$lang['email_smtp_auth_pw'] = 'Authenticate password, failed it did. Error: %s';
-$lang['email_smtp_data_failure'] = 'To send data, unable am I: %s';
-$lang['email_exit_status'] = 'Exit status code: %s. Yesss.';
+$lang['email_must_be_array']		= 'De validatiemethode voor email heeft er graag een array bij. Ja? Dus..';
+$lang['email_invalid_address']		= 'Dit emailadres wordt niet als zodanig beschouwd: %s';
+$lang['email_attachment_missing']	= 'Het lukt me niet om de bijlage die bij dit bericht hoort erbij te halen: %s';
+$lang['email_attachment_unreadable']	= 'Ik kan de bijlage die bij dit bericht hoort niet lezen. Ja? Dus: %s';
+$lang['email_no_from']			= 'Email versturen zonder "From"-header, dat doe ik niet. Ja?';
+$lang['email_no_recipients']		= 'Onthoudt: ontvangers (Naar, Cc, of Bcc) horen er ook bij.';
+$lang['email_send_failure_phpmail']	= 'Het lukt niet om email te versturen met PHP mail(). Deze server zou wel eens een andere methode gebruiken om berichten te sturen. Dus..';
+$lang['email_send_failure_sendmail']	= 'Het lukt niet om email te versturen met PHP Sendmail. Deze server zou wel eens een andere methode gebruiken om berichten te sturen. Dus..';
+$lang['email_send_failure_smtp']	= 'Het lukt niet om email te versturen met PHP SMTP.  Deze server zou wel eens een andere methode gebruiken om berichten te sturen. Dus..';
+$lang['email_sent']			= 'Het bericht is daadwerkelijk verstuurd, via het volgende protocol. Dus: %s';
+$lang['email_no_socket']		= 'Het lukt niet om een socket naar sendmail te openen.  Controleer instellingen.  Ja? hmmm.';
+$lang['email_no_hostname']		= 'Specificeer een SMTP hostnaam, die ontbrak nog.  ';
+$lang['email_smtp_error']		= 'Tegengehouden door een SMTP-foutmelding: %s';
+$lang['email_no_smtp_unpw']		= 'Fout: geef een SMTP gebruikersnaam met wachtwoord.';
+$lang['email_failed_smtp_login']	= 'De AUTH LOGIN opdracht gaf een foutmelding bij het versturen: %s';
+$lang['email_smtp_auth_un']		= 'Bij het authenticeren gaf de gebruikersnaam een foutmelding: %s';
+$lang['email_smtp_auth_pw']		= 'Bij het authenticeren gaf het wachtwoord een foutmelding:  %s';
+$lang['email_smtp_data_failure']	= 'Ik kan geen data versturen: %s';
+$lang['email_exit_status']		= 'Exit status code: %s. Ja?';
 
 /* End of file email_lang.php */
 /* Location: ./application/language/dutch/email_lang.php */
